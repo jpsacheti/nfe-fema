e3ee806432f72104a79ca2655fd4fd08aede5490
joao <jpsacheti@gmail.com>
2017-07-09 21:51:42 -0300

    Adicionado a estrutura inicial

---
+++ src/main/java/br/edu/fema/nfe/model/Endereco.java
@@ -0,0 +1,7 @@
+package br.edu.fema.nfe.model;
+
+/**
+ * Created by joao on 09/07/17.
+ */
+public class Endereco {
+}
---
+++ src/main/java/br/edu/fema/nfe/model/NotaFiscal.java
@@ -0,0 +1,9 @@
+package br.edu.fema.nfe.model;
+
+import java.time.LocalDateTime;
+
+public class NotaFiscal {
+	private LocalDateTime dataHoraEmissao;
+	private Integer codigo;
+	
+}
